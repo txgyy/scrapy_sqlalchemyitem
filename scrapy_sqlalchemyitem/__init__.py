e23b81cc1f777b976de88dc006789fb3c1728d4e
txgyy <tx2gyy@gmail.com>
2018-04-08 19:38:40 +0800

    增加了注释和sqlalchemy类型判断

--- scrapy_sqlalchemyitem/__init__.py
+++ scrapy_sqlalchemyitem/__init__.py
@@ -3,6 +3,8 @@
 
 from six import with_metaclass
 from scrapy.item import Field, Item, ItemMeta
+from sqlalchemy.ext.declarative.api import DeclarativeMeta
+from sqlalchemy.sql.schema import Table
 
 class SqlalchemyItemMeta(ItemMeta):
 
@@ -10,10 +12,16 @@ class SqlalchemyItemMeta(ItemMeta):
         cls = super(SqlalchemyItemMeta, mcs).__new__(mcs, class_name, bases, attrs)
         if cls.sqlalchemy_model:
             cls._model_fields = []
-            for model_field in cls.sqlalchemy_model.__table__.columns:
+            if isinstance(cls.sqlalchemy_model,DeclarativeMeta):
+                columns = cls.sqlalchemy_model.__table__.columns
+            elif isinstance(cls.sqlalchemy_model,Table):
+                columns = cls.sqlalchemy_model.columns
+            else:
+                raise TypeError('类型错误，仅支持 “DeclarativeMeta”，“Table” 类型')
+            for model_field in columns:
                     if model_field.name not in cls.fields:
                         cls.fields[model_field.name] = Field()
         return cls
-
+# with_metaclass 用meta创建base
 class SqlalchemyItem(with_metaclass(SqlalchemyItemMeta, Item)):
     sqlalchemy_model = None
